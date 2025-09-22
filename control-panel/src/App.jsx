1d7261630d81985aea528735c0f8959fdb20e0fa
saradaahalya <ahalyasarada@gmail.com>
2025-09-22 16:12:11 +0530

    feat: added ui

--- control-panel/src/App.jsx
+++ control-panel/src/App.jsx
@@ -19,6 +19,8 @@ const styles = `
     font-family: var(--font-family);
     background-color: var(--bg-color);
     color: var(--text-color);
+    width: 100%;
+    justify-content: center;
     margin: 0;
     padding: 2rem;
   }
